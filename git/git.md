e23007aae69b7e56fa356228e6e70db14eed13c9
rivalove <542763565@qq.com>
2019-04-29 16:16:20 +0800

    webpack

--- git/git.md
+++ git/git.md
@@ -6,3 +6,4 @@ git pull origin master
 git branch
 git checkout -b xxx / git checkout xxx
 git merge xxx
+git remote add origin
---
+++ webpack/src/app.js
@@ -0,0 +1,9 @@
+// var $ = require('jquery')
+
+// var $root = $('#root')
+
+// $root.html('<p>this is jquery sentense</p>')
+
+var utils = require('./a-util')
+
+utils.print()
