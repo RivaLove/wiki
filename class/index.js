c109186f11515a1d792aeb00d86b258d29a2ac67
rivalove <542763565@qq.com>
2019-04-30 22:31:59 +0800

    es6 解构复制 this环境的绑定 call

--- class/index.js
+++ class/index.js
@@ -1,12 +1,23 @@
-function AddHandle(a, b) {
-  this.a = a
-  this.b = b
-}
+// function AddHandle(a, b) {
+//   this.a = a
+//   this.b = b
+// }
 
-AddHandle.prototype.add = function () {
-  alert(this.a + this.b) 
-}
+// AddHandle.prototype.add = function () {
+//   alert(this.a + this.b) 
+// }
 
-var as = new AddHandle(1, 2)
+// var as = new AddHandle(1, 2)
 // console.log(as.add())
-as.add()
+// as.add()
+
+//this 测试
+
+function fn() {
+  console.log('fn', this);
+  [1, 2, 3].map((cur) => {
+    console.log('map', this);
+  })
+}
+
+fn.call({ a: 100 })
